68ac24888c9aaee9255662ff9ab7c76f7faf9e57
Gokulnath D <imgnath28@gmail.com>
2023-02-15 22:42:38 +0530

    Logging API requests

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 // Middleware
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.post(`${api}/products`, (req, res) => {
     const newProduct = req.body;
