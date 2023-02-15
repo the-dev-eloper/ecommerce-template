4de5b366c3c263827313c20b97aa360379f08f8a
Gokulnath D <imgnath28@gmail.com>
2023-02-15 22:33:20 +0530

    Create API call and parse json data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+app.post('/api/v1/products', (req, res) => {
+    const newProduct = req.body;
+    res.send(newProduct);
+})
+
 app.listen(3000, () => {
     console.log('App runs')
 });
