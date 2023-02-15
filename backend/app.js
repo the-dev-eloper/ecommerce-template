55d0efc35a6db2299faa1a20052599fb40153843
Gokulnath D <imgnath28@gmail.com>
2023-02-15 22:37:25 +0530

    Reading environment variables

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 // Middleware
 app.use(express.json());
 
-app.post('/api/v1/products', (req, res) => {
+app.post(`${api}/products`, (req, res) => {
     const newProduct = req.body;
     res.send(newProduct);
-})
+});
 
 app.listen(3000, () => {
     console.log('App runs')
