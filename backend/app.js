0930a1b43de5c093dc9519f7ef8cfdf936d7c6c3
Gokulnath D <imgnath28@gmail.com>
2023-02-15 23:44:12 +0530

    Connecting with MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,33 @@ const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 require('dotenv/config');
 
-const api = process.env.API_URL;
+mongoose.set("strictQuery", false);
 
 // Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
+const api = process.env.API_URL;
+
 app.post(`${api}/products`, (req, res) => {
     const newProduct = req.body;
     res.send(newProduct);
 });
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop'
+})
+    .then(()=>{
+        console.log('Database Connection is ready...')
+    })
+    .catch((err)=> {
+        console.log(err);
+    })
 
 app.listen(3000, () => {
     console.log('App runs')
