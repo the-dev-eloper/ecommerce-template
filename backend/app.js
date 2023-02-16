432b33aecb15c7dd5d2ff14f8c4c52dc7212c477
Gokulnath D <imgnath28@gmail.com>
2023-02-16 22:49:18 +0530

    Handle data to Database using API

--- backend/app.js
+++ backend/app.js
@@ -2,20 +2,47 @@ const express = require('express');
 const app = express();
 
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-require('dotenv/config');
 
+const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
+
+require('dotenv/config');
+const api = process.env.API_URL;
 
 // Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-const api = process.env.API_URL;
+const productModel = mongoose.Schema({
+    name: String,
+    price: Number,
+    countInStock: Number
+});
+const Product = mongoose.model('Product', productModel);
+
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(productList);
+});
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    res.send(newProduct);
+    const newProduct = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        countInStock: req.body.countInStock
+    });
+
+    newProduct.save()
+        .then(() => {
+            res.status(201).json({ success: true, product: newProduct });
+        })
+        .catch((err) => {
+            res.status(500).json({ success: false, error: err });
+        })
 });
 
 mongoose.connect(process.env.MONGODB_URL, {
@@ -23,10 +50,10 @@ mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology: true,
     dbName: 'eshop'
 })
-    .then(()=>{
+    .then(() => {
         console.log('Database Connection is ready...')
     })
-    .catch((err)=> {
+    .catch((err) => {
         console.log(err);
     })
 
