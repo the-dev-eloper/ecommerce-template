91147e04d2cb8459bbfc78ea1f4932529d73ac6f
Gokulnath D <imgnath28@gmail.com>
2023-02-25 20:18:18 +0530

    Enable CORS to backend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors());
 
 mongoose.set("strictQuery", false);
 const api = process.env.API_URL;
@@ -28,5 +32,5 @@ mongoose.connect(process.env.MONGODB_URL, {
     })
 
 app.listen(3000, () => {
-    console.log('App runs')
+    console.log('App runs');
 });
