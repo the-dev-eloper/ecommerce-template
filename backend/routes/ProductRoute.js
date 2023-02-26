e3e24e93dbe4f72d9602677293d3305f290b67c0
Gokulnath D <imgnath28@gmail.com>
2023-02-26 20:18:52 +0530

    API for Product count

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -85,4 +85,15 @@ router.delete(`/:id`, (req, res) => {
         })
 });
 
+router.get(`/get/count`, async (req, res) => {
+    const productCount = await Product.countDocuments();
+
+    if (!productCount) {
+        res.status(500).json({ success: false });
+    }
+    res.send({
+        productCount: productCount,
+    });
+});
+
 module.exports = router;
