9efdc9d7eba5254c7e6c12a83ba0deee3cdf57d1
Gokulnath D <imgnath28@gmail.com>
2023-02-26 19:42:39 +0530

    Update product API

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -45,4 +45,26 @@ router.post(`/`, async (req, res) => {
     res.send(newProduct);
 });
 
+router.put(`/:id`, async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    });
+
+    if(updatedProduct) {
+        res.status(200).json({ success: true, message: 'Product Updated Successfully'});
+    } else {
+        res.status(500).json({ success: false, message: 'Updation Failed'});
+    }
+});
+
 module.exports = router;
