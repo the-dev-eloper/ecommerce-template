203224bba5ab2d597eef294ff4b9bf977ca5e0be
Gokulnath D <imgnath28@gmail.com>
2023-02-27 22:45:39 +0530

    Filtering Products using category

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -5,7 +5,12 @@ const { Product } = require('../models/ProductModel');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find().populate('category');
+    let filter = {};
+    if(req.query.categories) {
+        filter = { category: req.query.categories.split(',')}
+    }
+
+    const productList = await Product.find(filter).populate('category');
 
     if(!productList) {
         res.status(500).json({ success: false });
