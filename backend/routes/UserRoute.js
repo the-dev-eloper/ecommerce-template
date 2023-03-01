5c45171414eabcf5ad8ca96ea6a9b2c0e9df4974
Gokulnath D <imgnath28@gmail.com>
2023-03-01 23:33:40 +0530

    API to get users without password

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -3,6 +3,15 @@ const { User } = require('../models/UserModel');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 
+router.get(`/`, async (req, res) =>{
+    const userList = await User.find().select('-passwordHash');
+
+    if(!userList) {
+        res.status(500).json({success: false});
+    }
+    res.send(userList);
+});
+
 router.post(`/register`, async (req, res) => {
     let newUser = new User({
         name: req.body.name,
