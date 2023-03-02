5579f8eb6add5e89e083d3642c88ef1371fef54f
Gokulnath D <imgnath28@gmail.com>
2023-03-02 23:06:13 +0530

    API to add a new user

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -12,6 +12,28 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 });
 
+router.post(`/`, async (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    });
+
+    newUser = await newUser.save();
+
+    if(!newUser)
+        return res.status(400).send('the user cannot be created!');
+
+    res.send(newUser);
+});
+
 router.post(`/register`, async (req, res) => {
     let newUser = new User({
         name: req.body.name,
