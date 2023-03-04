8b5feb6c68a8439797e93b9ed40691617f143d32
Gokulnath D <imgnath28@gmail.com>
2023-03-04 23:23:32 +0530

    Update User API

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -44,6 +44,39 @@ router.post(`/`, async (req, res) => {
     res.send(newUser);
 });
 
+router.put('/:id', async (req, res) => {
+    const userExist = await User.findById(req.params.id);
+    let newPassword;
+
+    if (req.body.password) {
+        newPassword = bcrypt.hashSync(req.body.password, 10);
+    } else {
+        newPassword = userExist.passwordHash;
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: newPassword,
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country,
+        },
+        { new: true }
+    );
+
+    if (!user)
+        return res.status(400).send('the user cannot be created!');
+
+    res.send(user);
+});
+
 router.delete('/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id).then(user => {
         if (user) {
