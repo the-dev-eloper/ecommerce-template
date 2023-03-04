abbe1d0cbd405fdbc047a72b7bd971eaf43f2c1c
Gokulnath D <imgnath28@gmail.com>
2023-03-04 17:21:27 +0530

    API to delete users

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -43,6 +43,18 @@ router.post(`/`, async (req, res) => {
     res.send(newUser);
 });
 
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user) {
+            return res.status(200).json({success: true, message: 'the user is deleted!'});
+        } else {
+            return res.status(404).json({success: false , message: "user not found!"});
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err});
+    });
+});
+
 router.post(`/register`, async (req, res) => {
     let newUser = new User({
         name: req.body.name,
